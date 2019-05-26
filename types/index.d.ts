e00738df480277ded2b160e8344e6c29621720d3
Aldarund <aldarund@gmail.com>
2019-05-26 14:26:23 +0300

    ts types fixes

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,20 +4,20 @@ import { TelemetryClient } from 'applicationinsights'
 // add type to Vue context
 declare module 'vue/types/vue' {
   interface Vue {
-    $appInsights: IApplicationInsights|TelemetryClient;
+    $appInsights: IApplicationInsights | TelemetryClient;
   }
 }
 
 // since nuxt 2.7.1 there is "NuxtAppOptions" for app context - see https://github.com/nuxt/nuxt.js/pull/5701
 declare module '@nuxt/vue-app' {
   interface NuxtAppOptions {
-    $appInsights: IApplicationInsights|TelemetryClient;
+    $appInsights: IApplicationInsights | TelemetryClient;
   }
 }
 
 // add types for Vuex Store
 declare module 'vuex/types' {
   interface Store<S> {
-    $appInsights: IApplicationInsights|TelemetryClient;
+    $appInsights: IApplicationInsights | TelemetryClient;
   }
 }
